65af95046a046354e3ab1eb8e21fbf47659b39bd
Chidera Mosanya <deramosanya@gmail.com>
2025-05-20 11:47:45 -0400

    relational conditions example

---
+++ src/main/java/com/kairos/java/refresher/intro/Demo.java
@@ -0,0 +1,13 @@
+package com.kairos.java.refresher.intro;
+
+public class Demo {
+    public static void main(String... args) {
+        int x = 7;
+        int y = 3;
+        int a = 15;
+        int b = 9;
+
+        boolean result = x > y || a < b;
+        System.out.println(!result);
+    }
+}
