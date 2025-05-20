30d5274b612bdeeaaac121685e7ca2a277290a52
Chidera Mosanya <deramosanya@gmail.com>
2025-05-20 12:13:15 -0400

    ternary operator

--- src/main/java/com/kairos/java/refresher/intro/Conditionals.java
+++ src/main/java/com/kairos/java/refresher/intro/Conditionals.java
@@ -8,11 +8,14 @@ public class Conditionals {
         Random generator = new Random();
         int x = generator.nextInt(50);
 
-        if(x > 10 && x % 10 == 0) {
+        if(x > 10 && x % 2 == 0) {
             System.out.printf("Hello... %d\n", x);
         }
         else {
             System.out.printf("Bye... %d", x);
         }
+
+        int n = 4;
+        int result = n % 2 == 0 ? 10 : 20;
     }
 }
