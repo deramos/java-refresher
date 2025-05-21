a27e8c69994dc4007b68b3c5a1d0bce8172198b7
Chidera Mosanya <deramosanya@gmail.com>
2025-05-21 09:38:26 -0400

    string experimentation

---
+++ src/main/java/com/kairos/java/refresher/strings/StringHandler.java
@@ -0,0 +1,8 @@
+package com.kairos.java.refresher.strings;
+
+public class StringHandler {
+    public static void main(String[] args) {
+        String name = "Chidera";
+        System.out.println(name.concat(" Mosanya"));
+    }
+}
