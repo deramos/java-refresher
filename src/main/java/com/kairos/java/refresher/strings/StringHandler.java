551356dea80b7407d1ec541ae5ace00b26b71fea
Chidera Mosanya <deramosanya@gmail.com>
2025-05-21 10:07:11 -0400

    playing around with stringbuilder and stringbuffer

--- src/main/java/com/kairos/java/refresher/strings/StringHandler.java
+++ src/main/java/com/kairos/java/refresher/strings/StringHandler.java
@@ -3,6 +3,19 @@ package com.kairos.java.refresher.strings;
 public class StringHandler {
     public static void main(String[] args) {
         String name = "Chidera";
+        boolean isCaller = true;
         System.out.println(name.concat(" Mosanya"));
+
+        StringBuffer buffer = new StringBuffer("Chidera");
+        buffer.append(" Mosanya");
+        System.out.println(buffer);
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Hello...");
+        builder.append(" World!");
+
+        System.out.println(builder.toString());
+
+        String st = buffer.toString();
     }
 }
