4af999f7fb93a4c8cc4053d033d83f5caa01e4ff
Chidera Mosanya <deramosanya@gmail.com>
2025-05-21 20:11:03 -0400

    created questions and question service

--- src/main/java/com/kairos/java/quizapp/Main.java
+++ src/main/java/com/kairos/java/quizapp/Main.java
@@ -2,6 +2,7 @@ package com.kairos.java.quizapp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        QuestionService questionService = new QuestionService();
+        questionService.displayQuestions();
     }
 }
--- src/main/java/com/kairos/java/quizapp/QuestionService.java
+++ src/main/java/com/kairos/java/quizapp/QuestionService.java
@@ -1,5 +1,17 @@
 package com.kairos.java.quizapp;
 
 public class QuestionService {
+    Question[] questions = new Question[3];
 
+    public QuestionService() {
+        questions[0] = new Question(1, "Hello bye", "A", "B", "C", "D", "B");
+        questions[1] = new Question(1, "Hello bye 2", "A", "B", "C", "D", "A");
+        questions[2] = new Question(1, "Hello bye 3", "A", "B", "C", "D", "C");
+    }
+
+    public void displayQuestions() {
+        for(Question question : questions){
+            System.out.println(question.getQuestion());
+        }
+    }
 }
