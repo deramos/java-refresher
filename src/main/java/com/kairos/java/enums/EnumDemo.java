bfcd443fdb75122c2fab188b5380e1aebccac4bc
Chidera Mosanya <deramosanya@gmail.com>
2025-05-23 14:55:31 -0400

    added experimental switch statement

--- src/main/java/com/kairos/java/enums/EnumDemo.java
+++ src/main/java/com/kairos/java/enums/EnumDemo.java
@@ -9,11 +9,34 @@ enum Status {
 
 public class EnumDemo {
     public static void main(String[] args) {
-        Status s = Status.Scheduled;
+        Status s = Status.Failed;
         System.out.println(s);
 
-        for(Status ss: Status.values()) {
-            System.out.println(ss);
+        if (s == Status.Scheduled) {
+            System.out.println("Status Scheduled");
+        } else if (s == Status.Running) {
+            System.out.println("Status Running");
+        } else if (s == Status.Failed) {
+            System.out.println("Status Failed");
+        } else {
+            System.out.println("Status Succeeded");
+        }
+
+        // Switch Statement with Enums
+        Status ss = Status.Succeeded;
+        switch (ss) {
+            case Scheduled:
+                System.out.println("Status Scheduled");
+                break;
+            case Running:
+                System.out.println("Status Running");
+                break;
+            case Failed:
+                System.out.println("Status Failed");
+                break;
+            case Succeeded:
+                System.out.println("Status Succeeded");
+                break;
         }
     }
 }
