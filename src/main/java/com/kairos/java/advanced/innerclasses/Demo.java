d5cecc8793ed9d2dcfa354cbd90aacbc8b3af8de
Chidera Mosanya <deramosanya@gmail.com>
2025-05-23 04:30:38 -0400

    experimenting with anonymous inner class

--- src/main/java/com/kairos/java/advanced/innerclasses/Demo.java
+++ src/main/java/com/kairos/java/advanced/innerclasses/Demo.java
@@ -1,5 +1,6 @@
 package com.kairos.java.advanced.innerclasses;
 
+
 class A {
     public void show() {
         System.out.println("In A. Show");
@@ -13,9 +14,19 @@ class A {
 }
 
 
+abstract class B {
+    abstract void show();
+}
+
 public class Demo {
     public static void main(String[] args) {
-        A.B b = new A.B();
+        B b = new B() {
+
+            @Override
+            void show() {
+                System.out.println("Showing inner B");
+            }
+        };
         b.show();
     }
 
