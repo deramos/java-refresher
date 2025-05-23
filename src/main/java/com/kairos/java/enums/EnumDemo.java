7f342b605ef6a09652073e1a242ca6aee3f88cd3
Chidera Mosanya <deramosanya@gmail.com>
2025-05-23 14:26:05 -0400

    experimenting with enum values

--- src/main/java/com/kairos/java/enums/EnumDemo.java
+++ src/main/java/com/kairos/java/enums/EnumDemo.java
@@ -11,5 +11,9 @@ public class EnumDemo {
     public static void main(String[] args) {
         Status s = Status.Scheduled;
         System.out.println(s);
+
+        for(Status ss: Status.values()) {
+            System.out.println(ss);
+        }
     }
 }
