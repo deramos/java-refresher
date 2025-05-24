c8f24fb8726b231111a544bb546983ec8095116d
Chidera Mosanya <deramosanya@gmail.com>
2025-05-24 18:34:23 -0400

    experminenting with Treemaps

--- src/main/java/com/kairos/java/advanced/collections/Demo.java
+++ src/main/java/com/kairos/java/advanced/collections/Demo.java
@@ -1,19 +1,27 @@
 package com.kairos.java.advanced.collections;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
+
+class Student {
+    Map<String, Integer> students;
+
+    public synchronized void addStudent(String name, int grade) {
+        students.put(name, grade);
+    }
+}
 
 public class Demo {
     public static void main(String[] args) {
-        Collection<Integer> nums = new ArrayList<>();
-        nums.add(6);
-        nums.add(5);
-        nums.add(4);
-        nums.add(3);
 
-        Iterator<Integer> iterator = nums.iterator();
-        while(iterator.hasNext())
-            System.out.println(iterator.next());
+        Map<String, Integer> students = new TreeMap<>();
+        students.put("Navin", 56);
+        students.put("Harsh", 45);
+        students.put("Sushil", 67);
+        students.put("Kiran", 45);
+
+
+        for(String name : students.keySet()) {
+            System.out.println(name + " : " + students.get(name));
+        }
     }
 }
