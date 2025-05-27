1b8c7f90228d7e35932d14e72d9c913e86261ab5
Chidera Mosanya <deramosanya@gmail.com>
2025-05-27 10:27:32 -0400

    experimenting with constructor reference

--- src/main/java/com/kairos/java/advanced/methodreference/MethodReferenceEx.java
+++ src/main/java/com/kairos/java/advanced/methodreference/MethodReferenceEx.java
@@ -1,15 +1,57 @@
 package com.kairos.java.advanced.methodreference;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+class Student {
+    private String name;
+    private int age;
+
+    public Student() {
+
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
 
 public class MethodReferenceEx {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Navin", "Chidera", "John");
-        List<String> uNames = names.stream().map(String::toUpperCase).toList();
 
-        uNames.forEach(System.out::println);
+        List<Student> students = new ArrayList<>();
 
+        for(String name : names) {
+            students.add(new Student(name));
+        }
+
+        System.out.println(students);
 
     }
 }
