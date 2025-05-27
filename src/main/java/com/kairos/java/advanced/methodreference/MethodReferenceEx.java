0b45f25e053e362cd0e302f035a7286688e41faf
Chidera Mosanya <deramosanya@gmail.com>
2025-05-27 10:09:26 -0400

    method reference experimenting

--- src/main/java/com/kairos/java/advanced/methodreference/MethodReferenceEx.java
+++ src/main/java/com/kairos/java/advanced/methodreference/MethodReferenceEx.java
@@ -6,8 +6,10 @@ import java.util.List;
 public class MethodReferenceEx {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Navin", "Chidera", "John");
-        List<String> uNames = names.stream().map(name -> name.toUpperCase()).toList();
+        List<String> uNames = names.stream().map(String::toUpperCase).toList();
 
-        System.out.println(uNames);
+        uNames.forEach(System.out::println);
+
+
     }
 }
