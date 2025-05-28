898aa72b2001fcc758e8d790870313c7be0037e7
Chidera Mosanya <deramosanya@gmail.com>
2025-05-28 12:40:40 -0400

    added entitymapping to hibernate.cfg.xml and alien entity

---
+++ src/main/java/com/kairos/java/hibernate/Alien.java
@@ -0,0 +1,44 @@
+package com.kairos.java.hibernate;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Alien {
+    private long aid;
+    private String name;
+    private String tech;
+
+
+    public long getAid() {
+        return aid;
+    }
+
+    public void setAid(long aid) {
+        this.aid = aid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTech() {
+        return tech;
+    }
+
+    public void setTech(String tech) {
+        this.tech = tech;
+    }
+
+    @Override
+    public String toString() {
+        return "Alien{" +
+                "aid=" + aid +
+                ", name='" + name + '\'' +
+                ", tech='" + tech + '\'' +
+                '}';
+    }
+}
