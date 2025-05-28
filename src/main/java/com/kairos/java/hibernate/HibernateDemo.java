827db0e4dc28764c605a6d65e1c3ac3502ab9521
Chidera Mosanya <deramosanya@gmail.com>
2025-05-28 11:24:28 -0400

    experimented with saving data in the DB using hibernate

--- src/main/java/com/kairos/java/hibernate/HibernateDemo.java
+++ src/main/java/com/kairos/java/hibernate/HibernateDemo.java
@@ -2,22 +2,30 @@ package com.kairos.java.hibernate;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateDemo {
     public static void main(String[] args) {
         Student s1 = new Student();
-        s1.setSid(7);
-        s1.setsName("Chidera");
-        s1.setMarks(89);
+        s1.setSid(11);
+        s1.setsName("Nkem");
+        s1.setMarks(90);
 
         Configuration configuration = new Configuration();
         configuration.addAnnotatedClass(com.kairos.java.hibernate.Student.class);
         configuration.configure();
 
-        SessionFactory factory = configuration.buildSessionFactory();
-        Session session = factory.openSession();
+        Session session;
+        try (SessionFactory factory = configuration.buildSessionFactory()) {
+            session = factory.openSession();
+        }
+
+        Transaction transaction = session.beginTransaction();
+
         session.persist(s1);
+
+        transaction.commit();
 
     }
 }
