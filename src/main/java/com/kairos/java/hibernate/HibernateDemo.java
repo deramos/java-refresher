b9bb7deae60de518b68f5c14d085ce5229a87bd2
Chidera Mosanya <deramosanya@gmail.com>
2025-05-28 11:53:39 -0400

    performed crud operations on hibernate

--- src/main/java/com/kairos/java/hibernate/HibernateDemo.java
+++ src/main/java/com/kairos/java/hibernate/HibernateDemo.java
@@ -8,9 +8,11 @@ import org.hibernate.cfg.Configuration;
 public class HibernateDemo {
     public static void main(String[] args) {
         Student s1 = new Student();
-        s1.setSid(11);
-        s1.setsName("Nkem");
-        s1.setMarks(90);
+        s1.setSid(12);
+        s1.setsName("Sally");
+        s1.setMarks(89);
+
+        Student s2 = null;
 
         Configuration configuration = new Configuration();
         configuration.addAnnotatedClass(com.kairos.java.hibernate.Student.class);
@@ -19,13 +21,15 @@ public class HibernateDemo {
         Session session;
         try (SessionFactory factory = configuration.buildSessionFactory()) {
             session = factory.openSession();
+
+            Transaction transaction = session.beginTransaction();
+
+            s2 = session.merge(s1);
+
+            transaction.commit();
+
+            System.out.println(s2);
         }
-
-        Transaction transaction = session.beginTransaction();
-
-        session.persist(s1);
-
-        transaction.commit();
 
     }
 }
